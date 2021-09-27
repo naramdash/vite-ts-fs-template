0062f0cfee709b95db7ddb7a7654931c2b8d16cc
naram.dash <juho_kim@outlook.com>
2021-09-27 10:37:27 +0900

    template init

---
+++ src/App.fs
@@ -0,0 +1,24 @@
+module App
+
+open Fable.Core.JsInterop
+open Fable.Core
+open Browser.Dom
+
+importAll "./style.css"
+
+// Member imports
+[<Import("alertPass", from = "./usets")>]
+let alertPass (msg1: string) (msg2: string) : unit = jsNative
+
+// Get a reference to our button and cast the Element to an HTMLButtonElement
+let app =
+    document.querySelector ("#app") :?> Browser.Types.HTMLDivElement
+
+// Register our listener
+app.innerHTML <-
+    """
+    <h1>Hello Vite!</h1>
+    <a href='https://vitejs.dev/guide/features.html' target='_blank'>Documentation</a>
+"""
+
+app.onclick <- fun _ -> alertPass "123" "abc"
---
+++ src/usets.ts
@@ -0,0 +1,5 @@
+function alertPass(msg1: string, msg2: string) {
+  alert(`msg1: ${msg1}\nmsg2: ${msg2}`);
+}
+
+export { alertPass };
